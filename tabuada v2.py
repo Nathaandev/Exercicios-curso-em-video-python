21364be1cbbb2b14ace6fd3f43e4242e08d52598
Nathan da Silva <111094286+Nathaandev@users.noreply.github.com>
2023-03-25 22:28:34 -0300

    Create tabuada v2.py

---
+++ tabuada v2.py
@@ -0,0 +1,13 @@
+escolha=int(input('Escolha um número: '))
+for c in range(1):
+    print(f'''
+    {escolha} x 1 = {escolha *1}
+    {escolha} x 2 = {escolha *2}
+    {escolha} x 3 = {escolha *3}
+    {escolha} x 4 = {escolha *4}
+    {escolha} x 5 = {escolha *5}
+    {escolha} x 6 = {escolha *6}
+    {escolha} x 7 = {escolha *7}
+    {escolha} x 8 = {escolha *8}
+    {escolha} x 9 = {escolha *9}
+    {escolha} x 10 = {escolha *10}''')
